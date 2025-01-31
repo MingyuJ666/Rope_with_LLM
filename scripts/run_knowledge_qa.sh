5f8fb5af0e2b96c79c39dc26d7edb20d60aff4bc
dongyuanjushi <kai.mei@rutgers.edu>
2025-01-31 10:41:07 -0500

    update

--- scripts/run_knowledge_qa.sh
+++ scripts/run_knowledge_qa.sh
@@ -1,4 +1,4 @@
-categories=("sports","art", "technology", "celebrity")
+categories=("sports", "art", "technology", "celebrity")
 
 for category in ${categories[@]}; do
     python datasets/create_knowledge_qa.py --category $category --num_pairs 200
